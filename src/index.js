49979acae5d4ae34ffa65451f5c2f28a7efad49f
Carlos Scala <carloss@awlatam.com>
2023-08-03 22:15:47 -0300

    deploy 3

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import ErrorPage from './Routes/Error/Error';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
